de37bc719b7a63c751e7663d167a5a8e62af8969
Hardy Lutula <hardylutula@gmail.com>
2022-07-15 12:52:34 +0200

    image added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ const App = () => {
   return (
     <div className='App'>
       <h1>Testing the App</h1>
+      <img src='https://images.unsplash.com/photo-1523217582562-09d0def993a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80' />
     </div>
   )
 }
